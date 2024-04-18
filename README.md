448e82839fe9bfc49a5b5d9a9d7535e5f994e4b7
Stela IS <stela.ishitani@gmail.com>
2024-04-17 20:03:55 -0400

    prepering master_file of microlensing data challenge.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # jasmine
-Joint Analysis of Simulation for Microlensing INterested Events (JASMINE)
+JASMINE: **J**oint **A**nalysis of **S**imulation for **M**icrolensing **IN**terested **E**vents
+
+---
+### Microlensing Data Challenge Simulations
+
+#### For Splitting Master File
+1. Make sure you downloaded the `master_file.txt` and `wfirstColumnNumbers.txt`  from the data challenge folder:  
+   https://github.com/microlensing-data-challenge/data-challenge-1/tree/master/Answers
+2. Run `python files_organizer/data_challenge_prep.py` , changing path as needed. It will split master file and create 4 new files:
+   * binary_star.csv
+   * bound_planet.csv
+   * cataclysmic_variables.csv
+   * single_lens.csv
+
+#### For reading the 4 csv files
+Call the function you need, and it will return a pandas dataframe:  
+`import files_organizer.data_challenge_reader as dcr`
+* `dataframe = dcr.binary_star_master_reader()`  
+* `dataframe = dcr.bound_planet_master_reader()`  
+* `dataframe = dcr.cataclysmic_variables_master_reader()`  
+* `dataframe = dcr.single_lens_master_reader()`  
+
+Obs: The column you are looking for is: `data_challenge_lc_number` . 
---
+++ files_organizer/data_challenge_prep.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import re
+
+
+def master_head_getter(path_to_wfirstcolumn_file_):
+    """
+    This function reads the wfirstcolumn file and returns a pandas dataframe with the columns
+    :param path_to_wfirstcolumn_file_:
+    :return:
+    """
+    wfirstcolumn_df = pd.read_csv(path_to_wfirstcolumn_file_, sep="\s+")
+    return wfirstcolumn_df
+
+
+def master_file_spliter(path_to_master_file_, path_to_wfirstcolumn_file_):
+    """
+    This function reads the master file and splits it into 4 different files based on the type of event.
+    :param path_to_master_file_:
+    :param path_to_wfirstcolumn_file_:
+    :return:
+    """
+    single_lens_lines = []  # dcnormffp
+    binary_star_lines = []  # ombin
+    bound_planet_lines = []  # omcassan
+    cataclysmic_variables_lines = []  # dccv
+    header_line = str(master_head_getter(path_to_wfirstcolumn_file_)['name'].values)
+    header_line = header_line.replace(' \'|\' ', ',').replace('\' \'', ',').replace('\'', '')
+    header_line = header_line.replace(' ', '').replace('|', '').replace('\n', ',')
+    header_line = header_line.replace('[','').replace(']','').replace(',,', ',')
+    print()
+    with open(path_to_master_file_, "r") as master_file:
+        for line in master_file:
+            line = line.replace(' | ', ' ')
+            if "Sky position" in line:
+                line = line.replace(' ', ' ')
+                line = re.sub("\s+", ",", line.strip())
+                master_header_line = line
+            else:
+                line = line.replace(' ', ',')
+                if "dcnormffp" in line:
+                    single_lens_lines.append(line)
+                elif "ombin" in line:
+                    binary_star_lines.append(line)
+                elif "omcassan" in line:
+                    bound_planet_lines.append(line)
+                elif "dccv" in line:
+                    cataclysmic_variables_lines.append(line)
+
+                else:
+                    raise ValueError("Unknown type of event\n", line)
+
+    with open('data/single_lens.csv', 'w') as single_lens_file:
+        single_lens_header_line = header_line+(',unimportant0,unimportant1,unimportant2,'
+                                               'event_type,unimportant3,lc_root,data_challenge_lc_number\n')
+        single_lens_file.write(single_lens_header_line)
+        for line in single_lens_lines:
+            single_lens_file.write(line)
+
+    with open('data/binary_star.csv', 'w') as binary_star_file:
+        binary_lens_header_line = header_line.replace(',normw,sigma_t0,sigma_tE,sigma_u0,sigma_alpha,sigma_s,'
+                                                      'sigma_q,sigma_rs,sigma_F00,sigma_fs0,sigma_F01,sigma_fs1,'
+                                                      'sigma_thetaE',
+                                                      ',unimportant0,event_type,unimportant1,lc_root,'
+                                                      'data_challenge_lc_number\n')
+        binary_star_file.write(binary_lens_header_line)
+        for line in binary_star_lines:
+            line = line.replace(',,,', ',')
+            binary_star_file.write(line)
+
+    with open('data/bound_planet.csv', 'w') as bound_planet_file:
+        bound_planet_header_line = header_line.replace(',normw,sigma_t0,sigma_tE,sigma_u0,sigma_alpha,sigma_s,'
+                                                       'sigma_q,sigma_rs,sigma_F00,sigma_fs0,sigma_F01,sigma_fs1,'
+                                                       'sigma_thetaE',
+                                                       ',unimportant0,event_type,unimportant1,lc_root,'
+                                                       'data_challenge_lc_number\n')
+        bound_planet_file.write(bound_planet_header_line)
+        for line in bound_planet_lines:
+            line = line.replace(',,,', ',')
+            bound_planet_file.write(line)
+
+    with open('data/cataclysmic_variables.csv', 'w') as cataclysmic_variables_file:
+        cataclysmic_variables_header_line = header_line.replace(',normw,sigma_t0,sigma_tE,sigma_u0,sigma_alpha,sigma_s,'
+                                                                'sigma_q,sigma_rs,sigma_F00,sigma_fs0,sigma_F01,'
+                                                                'sigma_fs1,'
+                                                                'sigma_thetaE',
+                                                                ',event_type,unimportant0,lc_root,'
+                                                                'data_challenge_lc_number\n')
+        cataclysmic_variables_file.write(cataclysmic_variables_header_line)
+        for line in cataclysmic_variables_lines:
+            cataclysmic_variables_file.write(line)
+
+
+if __name__ == '__main__':
+    # Write where you can find the master files
+    path_to_master_file = 'data/master_file.txt'
+    path_to_wfirstcolumn_file = 'data/wfirstColumnNumbers.txt'
+    master_file_spliter(path_to_master_file, path_to_wfirstcolumn_file)
+    print("Files have been created successfully")
+
+
