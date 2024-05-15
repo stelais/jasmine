a319770ce0b5379fe7b8504b099003c114f77f4f
Stela IS <stela.ishitani@gmail.com>
2024-05-15 13:56:15 -0400

    bound planets: q and s

--- jasmine/investigater/bound_planets.py
+++ jasmine/investigater/bound_planets.py
@@ -7,7 +7,7 @@ list_of_bound_planet_events = [4, 8, 12, 25, 32, 40, 47, 50, 53, 62, 66, 69, 74,
                                107, 124, 128, 131, 139, 152, 163, 186, 193, 194, 199, 208, 214, 217, 218, 223,
                                226, 227, 250, 253, 258, 267, 289]
 # list_of_bound_planet_events = [4,]
-master_path = '/local/data/emussd1/greg_shared/rtmodel_effort/datachallenge/datachallenge_events'
+master_path = '/local/data/emussd1/greg_shared/rtmodel_effort/datachallenge'
 # master_path = '/Users/sishitan/Documents/Scripts/jasmine'
 df_general = []
 for event_number in list_of_bound_planet_events:
