7d90bfa624627dc058d94b8444eac7594b6b8856
STELA ISHITANI <stela.ishitani@gmail.com>
2024-05-29 15:15:10 +0200

    fixing parallax_getter for parallax models only

--- jasmine/lightcurve_signal_generator/rtmodel_signal_generator.py
+++ jasmine/lightcurve_signal_generator/rtmodel_signal_generator.py
@@ -1,5 +1,6 @@
 from jasmine import RTModelTemplateForBinaryLightCurve
-
+import numpy as np
+import pandas as pd
 
 def easy_plot_lightcurve(rtmodel_template_two_lenses_):
     import matplotlib.pyplot as plt
@@ -9,11 +10,31 @@ def easy_plot_lightcurve(rtmodel_template_two_lenses_):
     plt.show(dpi=600)
 
 
+def all_rtmodel_template_generator(template_path, output_folder):
+    lines = np.arange(2, 114)
+    times_peak = np.arange(1, 402, 5)
+    times_peak_2 = np.arange(1, 402, 2)
+    base_times_peak_2 = np.arange(1, 402, 4)
+    np.random.shuffle(base_times_peak_2)
+    times_peak_2 = base_times_peak_2.append(shuffled_peak_2)
+
+    # intervals =
+    for line in lines:
+        rtmodel_template_two_lenses = RTModelTemplateForBinaryLightCurve(template_line=line,
+                                                                         path_to_template=template_path,
+                                                                         input_peak_t1=300,
+                                                                         input_peak_t2=302)
+        magnification, times = rtmodel_template_two_lenses.rtmodel_magnification_using_vbb()
+        binary_lens_signal_df = pd.DataFrame({'Magnification': magnification,
+                                              'times': times})
+        binary_lens_signal_df.to_csv(f'{output_folder}_rtmodel_{line}.csv')
+
+
 if __name__ == '__main__':
     template_path = '/Users/stela/Documents/Scripts/RTModel_project/RTModel/RTModel/data/TemplateLibrary.txt'
     rtmodel_template_two_lenses = RTModelTemplateForBinaryLightCurve(template_line=2,
                                                                      path_to_template=template_path,
                                                                      input_peak_t1=300,
                                                                      input_peak_t2=302)
-    easy_plot_lightcurve(rtmodel_template_two_lenses)
+    # easy_plot_lightcurve(rtmodel_template_two_lenses)
 
