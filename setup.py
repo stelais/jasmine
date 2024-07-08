129c9e9e47e59f83e9c17afb71a540b996ced14b
STELA ISHITANI <stela.ishitani@gmail.com>
2024-07-08 14:21:13 -0500

    New setup.py to include moana

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup
 
 setup(name='jasmine-astro',
-      version='0.1.2',
+      version='0.1.4.1',
       description='JASMINE: Joint Analysis of Simulations for Microlensing INterest Events',
       url='https://github.com/stelais/jasmine',
-      author='Stela IS',
+      author='Stela IS, Jon B., C.R., JASMINE authors',
       author_email='stela.ishitanisilva@nasa.gov',
       license='MIT',
       packages=['jasmine',
                 'jasmine.classes_and_files_reader',
                 'jasmine.files_organizer',
-                'jasmine.investigator'],
+                'jasmine.investigator',
+                'jasmine.lightcurve_signal_generator'],
+      platforms="Mac OS 14.5",
+      keywords=['Astronomy', 'Microlensing', 'Science'],
       zip_safe=False,
       install_requires=["pandas==2.1.4",
                         "jupyter==1.0.0",
@@ -20,5 +23,6 @@ setup(name='jasmine-astro',
                         "astropy==6.1.0",
                         "jplephem==2.22",
                         "bokeh==3.4.1",
-                        "VBBinaryLensing==3.7.0"
+                        "VBBinaryLensing==3.7.0",
+                        "moana-pypi==0.2"
                         ])
