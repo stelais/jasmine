cb87cb3d9fc83692dc486065b1f24080ee4fc6ba
Stela IS <stela.ishitani@gmail.com>
2024-09-16 18:41:05 -0400

    ra_dec_conversions from rtmodel to pylima

---
+++ jasmine/files_organizer/ra_and_dec_conversions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+# Function to convert RA from 'HH:MM:SS' to decimal degrees
+def ra_hms_to_deg(ra_hms):
+    h, m, s = [float(i) for i in ra_hms.split(':')]
+    ra_deg = 15 * (h + m / 60 + s / 3600)
+    return ra_deg
+
+
+# Function to convert Dec from 'DD:MM:SS' to decimal degrees
+def dec_dms_to_deg(dec_dms):
+    sign = -1 if dec_dms[0] == '-' else 1
+    d, m, s = [float(i) for i in dec_dms.split(':')]
+    dec_deg = sign * (abs(d) + m / 60 + s / 3600)
+    return dec_deg
