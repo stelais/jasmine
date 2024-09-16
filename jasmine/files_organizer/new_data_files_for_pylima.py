f03851b9da8e21c27b8c14b05cac2b8748f5f73c
Stela IS <stela.ishitani@gmail.com>
2024-09-16 19:10:13 -0400

    1.4.7.1 converting data to pylima readable format

--- jasmine/files_organizer/new_data_files_for_pylima.py
+++ jasmine/files_organizer/new_data_files_for_pylima.py
@@ -1,16 +1,26 @@
 import pandas as pd
 
-# Column names based on the file content
-column_names = ['Mag', 'err', 'HJD_minus_2450000']
 
-# Reading the data into a DataFrame, skipping the first line which is a header comment
-for filepath in ['RomanW149sat1.dat', 'RomanZ087sat2.dat']:
-    data = pd.read_csv(filepath, delim_whitespace=True, skiprows=1, names=column_names)
+def rtmodel_data_to_pylima(folder_path='None'):
+    # Column names based on the file content
+    column_names = ['Mag', 'err', 'HJD_minus_2450000']
+    if folder_path == 'None':
+        folder_path =''
+    else:
+        folder_path = folder_path + '/'
+    # Reading the data into a DataFrame, skipping the first line which is a header comment
+    for filepath in [f'{folder_path}RomanW149sat1.dat', f'{folder_path}RomanZ087sat2.dat']:
+        data = pd.read_csv(filepath, delim_whitespace=True, skiprows=1, names=column_names)
 
-    # Adding 2450000 to the third column
-    data['HJD'] = data['HJD_minus_2450000'] + 2450000
-    data = data.drop(columns=['HJD_minus_2450000'])
+        # Adding 2450000 to the third column
+        data['HJD'] = data['HJD_minus_2450000'] + 2450000
+        data = data.drop(columns=['HJD_minus_2450000'])
 
-    # Saving the modified data to a new CSV file
-    output_txt_path = f'{filepath.split(".")[0]}_pylima.dat'
-    data.to_csv(output_txt_path, index=False, sep=' ', header=False)
+        # Saving the modified data to a new CSV file
+        output_txt_path = f'{filepath.split(".")[0]}_pylima.dat'
+        data.to_csv(output_txt_path, index=False, sep=' ', header=False)
+
+
+if __name__ == '__main__':
+    rtmodel_data_to_pylima()
+    print('Data files created successfully.')
