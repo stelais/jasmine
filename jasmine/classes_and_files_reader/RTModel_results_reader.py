0ffb9e21e75d339e172646a0ee12e6e6c0301d7d
stelais <stela.ishitani@gmail.com>
2024-12-04 14:34:34 -0500

    [small fix] Analysis code for new gulls results

--- jasmine/classes_and_files_reader/RTModel_results_reader.py
+++ jasmine/classes_and_files_reader/RTModel_results_reader.py
@@ -5,42 +5,42 @@ import jasmine.classes_and_files_reader.mass_ratio_and_separation_getter as q_s_
 import jasmine.classes_and_files_reader.parallax_getter as pie_getter
 
 
-def chi2_getter(filepath):
+def chi2_getter(filepath_):
     """
     This function reads the chi2 file and returns the chi2 value
-    :param filepath:
+    :param filepath_:
     :return: chi2
     """
-    with open(filepath, 'r') as f:
+    with open(filepath_, 'r') as f:
         lines = f.readlines()
         chi2 = float(lines[0].split(' ')[-1])
     return chi2
 
 
-def models_per_chi2_rank(folder_path):
+def models_per_chi2_rank(folder_path_):
     """
     This function rank the models per chi2 value
     """
-    models = glob.glob(folder_path + '/Models/*txt')
-    models_names = [model.replace(folder_path, '').replace('/Models/', '').replace('.txt', '') for model in models]
+    models = glob.glob(folder_path_ + '/Models/*txt')
+    models_names = [model.replace(folder_path_, '').replace('/Models/', '').replace('.txt', '') for model in models]
     chi2_values = []
     for model in models:
         chi2_values.append(chi2_getter(model))
     chi2_df = pd.DataFrame({'model': models_names, 'chi2': chi2_values})
     chi2_df = chi2_df.sort_values(by='chi2')
-    chi2_df.to_csv(folder_path + '/Models/chi2_rank.csv', index=False)
+    chi2_df.to_csv(folder_path_ + '/Models/chi2_rank.csv', index=False)
     only_binary_lenses = chi2_df[chi2_df['model'].str.contains('L')]
-    only_binary_lenses.to_csv(folder_path + '/Models/chi2_rank_binary_lenses.csv', index=False)
-    only_LS = chi2_df[chi2_df['model'].str.contains('LS')]
-    only_LX = chi2_df[chi2_df['model'].str.contains('LX')]
-    only_LO = chi2_df[chi2_df['model'].str.contains('LO')]
-    top_1_of_each = pd.concat([only_LS.iloc[[0]], only_LX.iloc[[0]], only_LO.iloc[[0]]])
-    top_1_of_each.to_csv(folder_path + '/Models/chi2_top1_of_each_binary_lens_model.csv', index=False)
+    only_binary_lenses.to_csv(folder_path_ + '/Models/chi2_rank_binary_lenses.csv', index=False)
+    only_ls = chi2_df[chi2_df['model'].str.contains('LS')]
+    only_lx = chi2_df[chi2_df['model'].str.contains('LX')]
+    only_lo = chi2_df[chi2_df['model'].str.contains('LO')]
+    top_1_of_each = pd.concat([only_ls.iloc[[0]], only_lx.iloc[[0]], only_lo.iloc[[0]]])
+    top_1_of_each.to_csv(folder_path_ + '/Models/chi2_top1_of_each_binary_lens_model.csv', index=False)
 
 
-def main(general_path, list_of_events, parallax=True, mass_ratio_and_separation=True, type_of_event='top10_piE'):
-    for event in list_of_events:
-        folder_path_ = f'{general_path}/{event}'
+def main(general_path_, list_of_events_, parallax=True, mass_ratio_and_separation=True, type_of_event='top10_piE'):
+    for event in list_of_events_:
+        folder_path_ = f'{general_path_}/{event}'
         models_per_chi2_rank(folder_path_)
 
         if mass_ratio_and_separation:
@@ -49,10 +49,9 @@ def main(general_path, list_of_events, parallax=True, mass_ratio_and_separation=
             pie_getter.get_summary_of_parallax_per_event(folder_path_)
 
     if mass_ratio_and_separation:
-        q_s_getter.event_summary_q_s_wrapper(general_path, list_of_events, type_of_event)
+        q_s_getter.event_summary_q_s_wrapper(general_path_, list_of_events_, type_of_event)
     if parallax:
-        pie_getter.event_summary_parallax_wrapper(general_path, list_of_events, type_of_event)
-
+        pie_getter.event_summary_parallax_wrapper(general_path_, list_of_events_, type_of_event)
 
 
 if __name__ == '__main__':
@@ -69,7 +68,7 @@ if __name__ == '__main__':
     # # root_path = '/Users/sishitan/Documents/Scripts/RTModel_project/RTModel/datachallenge_events/'
 
     general_path = '/discover/nobackup/sishitan/orbital_task/RTModel_runs/top10_piE'
-    # general_path = '/Users/stela/Documents/Scripts/orbital_task/RTModel_runs/top10_piE'
+    # general_path_ = '/Users/stela/Documents/Scripts/orbital_task/RTModel_runs/top10_piE'
     list_of_events = ['event_0_1000_1445',
                       'event_0_42_2848',
                       'event_0_762_407',
@@ -81,3 +80,5 @@ if __name__ == '__main__':
                       'event_0_798_371',
                       'event_0_922_1199',
                       'event_1_755_564']
+
+    main(general_path, list_of_events, parallax=True, mass_ratio_and_separation=True, type_of_event='top10_piE')
