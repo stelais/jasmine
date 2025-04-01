7d5def3bc06332b155a8358f263ad993011efd3a
jonhulberg <brashearj@cua.edu>
2025-03-31 21:53:37 -0400

    Adds pylima MCMC script for RTModel results. Still some issues.
    Signed-off-by: jonhulberg <brashearj@cua.edu>

--- jasmine/files_organizer/new_data_files_for_pylima.py
+++ jasmine/files_organizer/new_data_files_for_pylima.py
@@ -14,7 +14,7 @@ def rtmodel_data_to_pylima(data_folder_path='None', data_output_folder_path='Non
         data_output_folder_path = data_output_folder_path + '/'
 
     # Reading the data into a DataFrame, skipping the first line which is a header comment
-    for filepath in ['RomanW149sat1.dat', 'RomanZ087sat2.dat']:
+    for filepath in ['RomanW146sat1.dat', 'RomanZ087sat2.dat','RomanK213sat3.dat']:
         data = pd.read_csv(f'{data_folder_path}{filepath}',
                            delim_whitespace=True,
                            skiprows=1,
@@ -28,7 +28,32 @@ def rtmodel_data_to_pylima(data_folder_path='None', data_output_folder_path='Non
         output_txt_path = f'{data_output_folder_path}{event_number:03}_{filepath.split(".")[0]}_pylima.dat'
         data.to_csv(output_txt_path, index=False, sep=' ', header=False)
 
+def rtmodel_data_to_pylima_data_challenge(data_folder_path='None', data_output_folder_path='None', event_number='None'):
+    # Column names based on the file content
+    column_names = ['Mag', 'err', 'HJD_minus_2450000']
+    if data_folder_path == 'None':
+        data_folder_path = ''
+    else:
+        data_folder_path = data_folder_path + '/'
+    if data_output_folder_path == 'None':
+        data_output_folder_path = ''
+    else:
+        data_output_folder_path = data_output_folder_path + '/'
 
+    # Reading the data into a DataFrame, skipping the first line which is a header comment
+    for filepath in ['RomanW149sat1.dat', 'RomanZ087sat2.dat']:
+        data = pd.read_csv(f'{data_folder_path}{filepath}',
+                           delim_whitespace=True,
+                           skiprows=1,
+                           names=column_names)
+
+        # Adding 2450000 to the third column
+        data['HJD'] = data['HJD_minus_2450000'] + 2450000
+        data = data.drop(columns=['HJD_minus_2450000'])
+
+        # Saving the modified data to a new CSV file
+        output_txt_path = f'{data_output_folder_path}{event_number:03}_{filepath.split(".")[0]}_pylima.dat'
+        data.to_csv(output_txt_path, index=False, sep=' ', header=False)
 if __name__ == '__main__':
     rtmodel_data_to_pylima()
     print('Data files created successfully.')
