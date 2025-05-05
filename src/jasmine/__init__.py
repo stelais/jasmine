7b08a35ce5b12b762217071c03804e76b824e012
stelais <stela.ishitani@gmail.com>
2025-05-05 15:41:42 -0400

    fix for incorrecting renaming inside modules from src.jasmine to jasmine

--- src/jasmine/__init__.py
+++ src/jasmine/__init__.py
@@ -1,4 +1,4 @@
-from src.jasmine.classes_and_files_reader.RTModel_results_cls import ModelResults
+from jasmine.classes_and_files_reader.RTModel_results_cls import ModelResults
 
 __all__ = ['ModelResults', 'LightcurveEventDataChallenge', 'RTModelTemplateForBinaryLightCurve']
 
