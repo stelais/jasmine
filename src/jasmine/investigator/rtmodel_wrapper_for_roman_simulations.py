c971ffca937bc9e9d7580e49f1b4a5c71b61ab80
stelais <stela.ishitani@gmail.com>
2025-10-01 09:48:41 -0300

    [temp fix] Adding option if initial condition grid search option

--- src/jasmine/investigator/rtmodel_wrapper_for_roman_simulations.py
+++ src/jasmine/investigator/rtmodel_wrapper_for_roman_simulations.py
@@ -5,10 +5,13 @@ from jasmine import ModelResults
 from jasmine.classes_and_files_reader import RTModel_results_cls as rtm_results
 
 
-def collect_event_summary(event_result):
+def collect_event_summary(event_result, is_ICGS=False):
     """ Collects all parameters from an event result object."""
     data = {"event_name": event_result.event_name}
-    for category in ["PS", "PX", "BS", "BO", "LS", "LX", "LO"]:
+    categories = ["PS", "PX", "BS", "BO", "LS", "LX", "LO"]
+    if is_ICGS:
+        categories = ["PS", "PX", "LS", "LX", "LO"]
+    for category in categories:
         best_obj = getattr(event_result, f"{category.lower()}_best")
         best_category_model_name = best_obj.name
         data[f"{category.upper()}_name"] = best_category_model_name
@@ -17,12 +20,14 @@ def collect_event_summary(event_result):
         # Dynamically add all attributes from model_parameters
         for attr, value in vars(model_results.model_parameters).items():
             data[f"{category.upper()}_{attr}"] = value
+        print()
     return data
 
 
-def loop_through_all_the_folders(folder_with_runs_path_):
+def loop_through_all_the_folders(folder_with_runs_path_, is_ICGS=False):
     all_event_summaries_ = []
     for folder in Path(folder_with_runs_path_).glob("*"):
+        print(folder.name)
         if not folder.is_dir():
             continue
         try:
@@ -30,7 +35,7 @@ def loop_through_all_the_folders(folder_with_runs_path_):
             event_result.extract_information_from_nature_file()
             event_result.looking_for_the_names_of_best_model_of_each_category()
 
-            event_data = collect_event_summary(event_result)
+            event_data = collect_event_summary(event_result, is_ICGS)
 
             event_data['complete_classification'] = event_result.complete_classification
             event_data['number_of_final_models'] = event_result.number_of_final_models
@@ -106,12 +111,17 @@ if __name__ == "__main__":
     # # true_and_rtmodel_fits_path = '/Users/stela/Documents/Scripts/orbital_task/RTModel_runs/sample_rtmodel_v2.4/true_and_rtmodel_fits.csv'
     # true_and_rtmodel_fits_path = '/gpfsm/dnb34/sishitan/orbital_task/RTModel_runs/269_problematic_events/true_and_rtmodel_fits.csv'
     # Define the paths
-    folder_with_runs_path = '/gpfsm/dnb34/sishitan/orbital_task/RTModel_runs/sample_rtmodel_v3.1/'
-    model_output_table_path = '/gpfsm/dnb34/sishitan/orbital_task/RTModel_runs/sample_rtmodel_v3.1/all_models_summary.csv'
-    master_file = '/gpfsm/dnb34/sishitan/orbital_task/data/gulls_orbital_motion_extracted/OMPLDG_croin_cassan.sample.csv'
-    true_and_rtmodel_fits_path = '/gpfsm/dnb34/sishitan/orbital_task/RTModel_runs/sample_rtmodel_v3.1/true_and_rtmodel_fits.csv'
+    # folder_with_runs_path = '/gpfsm/dnb34/sishitan/orbital_task/RTModel_runs/sample_rtmodel_v3.1/'
+    is_this_a_ICGS_run = True
+    root_path = '/Users/stela/Documents/Scripts/orbital_task'
+    # root_path = '/gpfsm/dnb34/sishitan/orbital_task'
+    folder_with_runs_path = f'{root_path}/RTModel_runs/154_failures_v24_v31/'
+    model_output_table_path = f'{root_path}/RTModel_runs/154_failures_v24_v31/all_models_summary.csv'
+    master_file = f'{root_path}/data/gulls_orbital_motion_extracted/OMPLDG_croin_cassan.sample.csv'
+    true_and_rtmodel_fits_path = f'{root_path}/RTModel_runs/154_failures_v24_v31/ICGS_true_and_rtmodel_fits.csv'
+
     
-    all_event_summaries = loop_through_all_the_folders(folder_with_runs_path)
+    all_event_summaries = loop_through_all_the_folders(folder_with_runs_path, is_ICGS=is_this_a_ICGS_run)
     model_summary_df = pd.DataFrame(all_event_summaries)
     model_summary_df.to_csv(model_output_table_path, index=False)
     print(f"✅ Saved {len(model_summary_df)} events to {model_output_table_path}")
@@ -162,6 +172,41 @@ if __name__ == "__main__":
         'LO_gamma1', 'LO_gamma1_error', 'LO_gamma2', 'LO_gamma2_error', 'LO_gammaz', 'LO_gammaz_error',
         'LO_blends', 'LO_sources', 'LO_blendings', 'LO_blendings_error', 'LO_baselines',
         'LO_baselines_error']
+    if is_this_a_ICGS_run:
+        order_of_columns_to_be_saved = [
+            'event_name',
+            'complete_classification', 'found_a_planetary_solution', 'found_a_2L1S_solution',
+            'number_of_final_models', 'final_models',
+            'true_mass_ratio', 'true_separation', 'true_piEN', 'true_piEE', 'true_u0', 'true_t0', 'true_tE',
+            'true_rho', 'true_alpha', 'true_planet_inclination', 'true_planet_orbphase', 'true_planet_period',
+            'true_ObsGroup_0_chi2', 'true_ObsGroup_0_flatchi2',
+            'PS_name', 'PS_chi2', 'PS_number_of_parameters', 'PS_u0', 'PS_u0_error', 'PS_tE', 'PS_tE_error', 'PS_t0',
+            'PS_t0_error',
+            'PS_rho', 'PS_rho_error', 'PS_blends', 'PS_sources', 'PS_blendings', 'PS_blendings_error',
+            'PS_baselines', 'PS_baselines_error',
+            'PX_name', 'PX_chi2', 'PX_number_of_parameters', 'PX_u0', 'PX_u0_error', 'PX_tE', 'PX_tE_error', 'PX_t0',
+            'PX_t0_error',
+            'PX_rho', 'PX_rho_error', 'PX_piEN', 'PX_piEN_error', 'PX_piEE', 'PX_piEE_error',
+            'PX_blends', 'PX_sources', 'PX_blendings', 'PX_blendings_error', 'PX_baselines', 'PX_baselines_error',
+            'LS_name', 'LS_chi2', 'LS_number_of_parameters', 'LS_separation', 'LS_separation_error', 'LS_mass_ratio',
+            'LS_mass_ratio_error', 'LS_mass_ratio_diff',
+            'LS_u0', 'LS_u0_error', 'LS_alpha', 'LS_alpha_error', 'LS_rho', 'LS_rho_error', 'LS_tE', 'LS_tE_error',
+            'LS_t0', 'LS_t0_error', 'LS_blends', 'LS_sources', 'LS_blendings', 'LS_blendings_error',
+            'LS_baselines', 'LS_baselines_error',
+            'LX_name', 'LX_chi2', 'LX_number_of_parameters', 'LX_separation', 'LX_separation_error', 'LX_mass_ratio',
+            'LX_mass_ratio_error', 'LX_mass_ratio_diff',
+            'LX_u0', 'LX_u0_error', 'LX_alpha', 'LX_alpha_error', 'LX_rho', 'LX_rho_error', 'LX_tE', 'LX_tE_error',
+            'LX_t0', 'LX_t0_error', 'LX_piEN', 'LX_piEN_error', 'LX_piEE', 'LX_piEE_error', 'LX_blends',
+            'LX_sources',
+            'LX_blendings', 'LX_blendings_error', 'LX_baselines', 'LX_baselines_error',
+            'LO_name', 'LO_chi2', 'LO_number_of_parameters', 'LO_separation', 'LO_separation_error', 'LO_mass_ratio',
+            'LO_mass_ratio_error', 'LO_mass_ratio_diff',
+            'LO_u0', 'LO_u0_error', 'LO_alpha', 'LO_alpha_error', 'LO_rho', 'LO_rho_error', 'LO_tE',
+            'LO_tE_error',
+            'LO_t0', 'LO_t0_error', 'LO_piEN', 'LO_piEN_error', 'LO_piEE', 'LO_piEE_error',
+            'LO_gamma1', 'LO_gamma1_error', 'LO_gamma2', 'LO_gamma2_error', 'LO_gammaz', 'LO_gammaz_error',
+            'LO_blends', 'LO_sources', 'LO_blendings', 'LO_blendings_error', 'LO_baselines',
+            'LO_baselines_error']
     # check if all columns are present
     missing_columns = [col for col in order_of_columns_to_be_saved if col not in final_df.columns]
     if missing_columns:
@@ -170,7 +215,10 @@ if __name__ == "__main__":
         print("✅ All dcolumns are present in the DataFrame.")
     final_df = final_df[order_of_columns_to_be_saved]
     # excluding some columns that are not needed
-    for category in ['PS', 'PX', 'BS', 'BO', 'LS', 'LX', 'LO']:
+    categories = ['PS', 'PX', 'BS', 'BO', 'LS', 'LX', 'LO']
+    if is_this_a_ICGS_run:
+        categories = ['PS', 'PX', 'LS', 'LX', 'LO']
+    for category in categories:
         final_df = final_df.drop(columns=[f'{category}_blends',
                                          f'{category}_sources', f'{category}_blendings',
                                             f'{category}_blendings_error', f'{category}_baselines',
