340bf76e38e61b654c5a4e82869118e9c68c4736
Stela <40497409+stelais@users.noreply.github.com>
2026-03-31 15:48:59 -0400

    Rmdc26 (#25)
    * Adding class that reads Roman Microlensing Data Challenge 2026 parquet file
    
    * NEEDS pyarrow + adding pre-commit
    
    * uploading pre-commit
    
    * small comment + formatting
    
    * Python routine that creates files ready to be run by RTModel
    
    * Includes routine that reads Roman Microlensing Data Challenge 2026 and transform the data for RTModel style
    
    * Includes routine that reads Roman Microlensing Data Challenge 2026 and transform the data for RTModel style
    
    * fixing suffix from `.txt` to `.dat`
    
    * rebasing + two comments

--- src/jasmine/files_organizer/RMDC26_to_RTModel.py
+++ src/jasmine/files_organizer/RMDC26_to_RTModel.py
@@ -35,7 +35,7 @@ def creating_rtmodel_directories(
 
         # Iterate over available filters in simple_lightcurve
         for filt, lc_df in event.simple_lightcurve.items():
-            save_path = os.path.join(event_dir, f"{filt}_.txt")
+            save_path = os.path.join(event_dir, f"{filt}_.dat")
 
             # Write header manually and then the data
             with open(save_path, "w") as f:
@@ -63,6 +63,10 @@ def creating_rtmodel_directories(
         else:
             with open(f"{event_dir}/event.coordinates", "w") as f:
                 f.write(f"{rtmodel_event_coordinates}")
+
+        # Add limbdarkening in the future
+        # Add satellite coordinates in the future
+
         print(f"{event_dir} conclude!")
 
 
